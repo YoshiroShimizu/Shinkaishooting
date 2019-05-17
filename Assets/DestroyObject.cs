1e3e2805e6783748b46d3684a8c6c5b69f4bdb26
yoshiroX <roboxpress@yahoo.co.jp>
2019-05-18 07:22:08 +0900

    エネミーにあたり判定を追加

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+    public int damage;
+    private GameObject enemy;
+    private HP hp;
+
+
+	// Use this for initialization
+	void Start () {
+
+        enemy = GameObject.Find("ENEMY");
+        hp = enemy.GetComponent<HP>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("shell"))
+        {
+            //HPクラスのダメージ関数を呼び出してダメージを与える
+            hp.Damage(damage);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
