5b50d98c24ad05dbdbaac793aefc705b3ac52f45
yoshiroX <roboxpress@yahoo.co.jp>
2019-05-25 06:51:18 +0900

    ドア作成完了

---
+++ Assets/SwitchOfDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchOfDoor : MonoBehaviour
+{
+
+    public GameObject RedDoor;
+    public GameObject GreenDoor;
+    public GameObject BlueDoor;
+    public GameObject RedDoorSwitch;
+    public GameObject GreenDoorSwitch;
+    public GameObject BlueDoorSwitch;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("shell") && this.gameObject == RedDoorSwitch)
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+            Destroy(RedDoor, 2.0f);
+        }
+        else if (other.CompareTag("shell") && this.gameObject == GreenDoorSwitch)
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+            Destroy(GreenDoor, 2.0f);
+        }
+        else if (other.CompareTag("shell") && this.gameObject == BlueDoorSwitch)
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+            Destroy(BlueDoor, 2.0f);
+        }
+    }
+}
